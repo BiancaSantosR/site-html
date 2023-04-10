eb5959a428b1bd81e7972fe37d9e2ce6ffb4aaca
renancreuso <renan_almeida@ufms.br>
2023-04-10 12:50:15 -0300

    feito alguns ajuste

---
+++ script/script.js
@@ -0,0 +1,19 @@
+const botaoLogin = document.querySelector("[data-botao]");
+
+botaoLogin.addEventListener("click", (evento) => {
+
+    evento.preventDefault();
+
+    const user = document.getElementById('user').value;
+    const senha = document.getElementById('password').value;
+
+    if (user == "admin" && senha == "admin") {
+        alert('Sucesso');
+        location.href = "../index.html";
+    } else {
+        alert('Usuario ou senha incorretos');
+    }
+
+    return false
+})
+
